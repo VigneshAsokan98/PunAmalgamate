a6105603ab284f1887a7a045a84bcd143aab2633
VigneshAsokan98 <93536574+VigneshAsokan98@users.noreply.github.com>
2022-09-27 01:11:51 +0100

    Lobby Added

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -7,30 +7,34 @@ using System;
 
 public class NetworkManager : MonoBehaviourPunCallbacks
 {
-    // Start is called before the first frame update
-    void Start()
+    int SceneIdx = 1;
+    private void Start()
     {
-        connectToServer();
+        SceneIdx = PlayerPrefs.GetInt("CurrentLevelIdx", 1);
     }
-
-    private void connectToServer()
+    public void connectToServer()
     {
         PhotonNetwork.ConnectUsingSettings();
         Debug.Log("Initialing Connection!");
-
     }
 
     public override void OnConnectedToMaster()
     {
         Debug.Log("Connected to Server");
         base.OnConnectedToMaster();
+        InitRoom(); 
+    }
+    public void InitRoom()
+    {
+        //Loading Scene
+        PhotonNetwork.LoadLevel(SceneIdx);
+
         RoomOptions roomOptions = new RoomOptions();
         roomOptions.MaxPlayers = 2;
         roomOptions.IsOpen = true;
         roomOptions.IsVisible = true;
         PhotonNetwork.JoinOrCreateRoom("Room 1", roomOptions, TypedLobby.Default);
     }
-
     public override void OnJoinedRoom()
     {
         Debug.Log("Room Joined");
